d0f7f2f50f750da68267179a43b014b2289cd008
adraychen <68781407+adraychen@users.noreply.github.com>
2025-07-30 01:33:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Hugging Face inference endpoint
-HF_API_URL = "https://api-inference.huggingface.co/embeddings/sentence-transformers/all-MiniLM-L6-v2"
+HF_API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
+
 
 
 def get_embedding(text: str) -> List[float]:
